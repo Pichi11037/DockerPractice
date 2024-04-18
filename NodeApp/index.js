918c8db646d858ebe65cf23d08d2b15215737170
Andres Pichimata <afpichimata@hotmail.com>
2024-04-18 18:25:47 -0500

    adding changes before reseting pc

---
+++ NodeApp/index.js
@@ -0,0 +1,11 @@
+const express = require('expres');
+
+const app = express();
+
+app.get('/', (req,res) => {
+	res.send('Hi there');
+})
+
+app.listen(8080, () =>{
+	console.log('Listening on port 8080');
+});
